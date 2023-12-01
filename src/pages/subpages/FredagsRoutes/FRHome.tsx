aa4cefd0a2eaa0b3022000535adc92d2ffba45d3
doctorbond2 <michael.rostlund@hotmail.com>
2023-12-01 10:45:36 +0100

    asd

--- src/pages/subpages/FredagsRoutes/FRHome.tsx
+++ src/pages/subpages/FredagsRoutes/FRHome.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FRInter } from "../../../types/TodoTypes";
 import FRUserList from "../../../components/FredagsRoutes/FRUserList";
 type Props = {
